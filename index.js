910bea5656b3d64e956e91825b644281e24d843a
shad mustafa <shadmanmi2018@gmail.com>
2022-10-30 17:54:52 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
+  function superbowlWin(avarages){
+
+    for(const avarage of avarages){
+
+if(avarage.result==='W'){
+    return avarage.year
+}
+
+}
+  }
+
+
+ 
+    record.find(superbowlWin(record));
+
+  
+  
